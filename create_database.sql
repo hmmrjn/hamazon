ac788da79b01732e482659ac33a3e735f24d5c39
hmmrjn <rodhamjun@hotmail.com>
2016-09-08 20:05:35 +0900

    create_database.sqlに欠陥があったので修正

--- create_database.sql
+++ create_database.sql
@@ -1,11 +1,11 @@
--- このファイルでテーブルをデータベスインポートしてください。
+-- このファイルを使ってデータベースの構造と商品データをインポートできます。
 
 -- phpMyAdmin SQL Dump
 -- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016 年 9 月 04 日 01:16
+-- Generation Time: 2016-9-7 23:29
 -- サーバのバージョン： 5.6.24
 -- PHP Version: 5.6.8
 
@@ -21,20 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `hamazon`
 --
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `customers`
---
-
-CREATE TABLE IF NOT EXISTS `customers` (
-  `id` varchar(30) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `mail` varchar(30) NOT NULL,
-  `pass` varchar(30) NOT NULL,
-  `reg_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -60,23 +46,8 @@ CREATE TABLE IF NOT EXISTS `items` (
 
 INSERT INTO `items` (`id`, `name`, `kana`, `deleted`, `price`, `category`, `date`, `details`, `stock`) VALUES
 (1, '東京電機大学千住キャンパス1号館', 'とうきょうでんきだいがくせんじゅきゃんぱす１ごうかん', 0, 500000000, '1', '2015-06-25', '最先端設備を備え、最新技術を駆使して省CO2エコキャンパスを実現するとともに、免震、制震、非常用設備など防災機能を充実させたキャンパスです。キャンパス内の図書館やカフェ、3つのプラザなど地域の人々にも開放され、人と緑にあふれた優しいまちづくりにも積極的に参加していきます。勉強の合間の気分転換にピッタリなルーフガーデン、そしてカフェラウンジ、食堂は、どの建物からでも行きやすい場所に配置。学生の過ごしやすさを第一に考えたキャンパスです。（製造者より）', 1),
-(2, '東京電機大学千住キャンパス2号館', 'とうきょうでんきだいがくせんじゅきゃんぱす2ごうかん', 0, 400000000, '1', '2016-03-28', '2号館です。詳細は知りません。', 10),
-(3, '東京電機大学千住キャンパス3号館', 'とうきょうでんきだいがくせんじゅきゃんぱす3ごうかん', 0, 400000000, '1', '2016-03-29', '2号館です。詳細は知りません。', 10),
-(4, 'タカセ丸　蓮タロウ®', '', 0, 2980, '', '2016-07-31', '', 100),
-
--- --------------------------------------------------------
-
---
--- テーブルの構造 `order_details`
---
-
-CREATE TABLE IF NOT EXISTS `order_details` (
-  `detail_id` int(10) NOT NULL,
-  `order_id` int(10) NOT NULL,
-  `item_id` int(10) NOT NULL,
-  `priced` int(10) NOT NULL,
-  `quantity` int(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+(2, '東京電機大学千住キャンパス2号館', 'とうきょうでんきだいがくせんじゅきゃんぱす2ごうかん', 0, 400000000, '1', '2016-03-28', '2号館です。北千住動物公園が備わっています。', 10),
+(3, '東京電機大学千住キャンパス3号館', 'とうきょうでんきだいがくせんじゅきゃんぱす3ごうかん', 0, 400000000, '1', '2016-03-29', '3号館です。名物ストリームスポーツ「席確保」のホームスタジアムです。', 20);
 
 -- --------------------------------------------------------
 
@@ -90,6 +61,18 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `date` date NOT NULL,
   `total_price` int(10) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルの構造 `order_details`
+--
+
+CREATE TABLE IF NOT EXISTS `order_details` (
+  `detail_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `item_id` int(10) NOT NULL,
+  `priced` int(10) NOT NULL,
+  `quantity` int(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルの構造 `reviews`
@@ -106,8 +89,6 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   `likes` int(4) NOT NULL DEFAULT '0',
   `dislikes` int(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
 
 --
 -- テーブルの構造 `users`
@@ -135,16 +116,16 @@ ALTER TABLE `items`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `order_details`
---
-ALTER TABLE `order_details`
-  ADD PRIMARY KEY (`detail_id`);
-
---
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`detail_id`);
 
 --
 -- Indexes for table `reviews`
@@ -166,22 +147,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `order_details`
---
-ALTER TABLE `order_details`
-  MODIFY `detail_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `order_id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT for table `reviews`
---
-ALTER TABLE `reviews`
-  MODIFY `review_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
