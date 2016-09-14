e5bd8e249211cbae6a12b091348dcd213e9fc436
hmmrjn <rodhamjun@hotmail.com>
2016-09-14 23:25:43 +0900

    スマホの場合はフッターのボタンのテキストを非表示に

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,9 @@
 <aside>
 <div id="counter"><img src="http://goo.gl/oGoFL9" alt="たくさんの訪問ありがとうございます！" border="0" /></div>
 <ul>
-<a href="/contact/" class="btn"><i class="fa fa-envelope"></i>問い合わせ</a>
-<a href="http://twitter.com/hamazon103" class="btn"><i class="fa fa-twitter"></i>@hamazon103</a>
-<a href="https://github.com/hmmrjn/hamazon" id="github-btn" class="btn"><i class="fa fa-github"></i>GitHub</a>
+<a href="/contact/" class="btn"><i class="fa fa-envelope"></i><span class="extra-label">問い合わせ</span></a>
+<a href="http://twitter.com/hamazon103" class="btn"><i class="fa fa-twitter"></i><span class="extra-label">@hamazon103</span></a>
+<a href="https://github.com/hmmrjn/hamazon" id="github-btn" class="btn"><i class="fa fa-github"></i><span class="extra-label">GitHub</span></a>
 </ul>
 <p id="legal">"Hamazon"は東京電機大学の登録商標ではありません。このサイトはフィクションであり、実在の人物や団体などとは関係ありません。
 <br>© Copyright 2016 Hamazon Co. Ltd. by 15fi103 Some Rights Reserved. <a href="TermsOfUse.html">利用規約</a> <a href="PrivacyPolicies.html">プライバシー規約</a> (準備中)</p>
